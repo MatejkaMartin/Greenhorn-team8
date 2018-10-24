4d6652bdd2f299d3db911e1583f1ad4ddc3f68ef
Martin Matějka <martinmatejka@me.com>
2018-10-24 23:12:22 +0200

    A-0: Removed styled-components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.jpg';
 import './App.css';
-import { Title2, Text } from './theme.js';
 import LoginButton from './components/LoginButton.js'
-import styled from 'styled-components';
 import AppBar from './components/AppBar.js'
 import withRootTemplate from './withRootTemplate';
 
@@ -17,8 +15,6 @@ class App extends Component {
             <p>This is GreenHorn!</p>
         </header>
         <LoginButton> Login bitch</LoginButton>
-        <Title2>This is test </Title2>
-        <Text color='#000000'> This is test of text color </Text>
       </div>
     );
   }
--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -2,32 +2,15 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import SaveIcon from '@material-ui/icons/Save';
-import { StyledButton } from './../theme.js'
 import classNames from 'classnames';
 import green from '@material-ui/core/colors/green';
 
-//This is example if we would like to configure extra style for buttom
-const styles = theme => ({
-  button: {
-    color: green,
-    margin: theme.spacing.unit,
-  },
-  rightIcon: {
-    marginLeft: theme.spacing.unit,
-  },
-  iconSmall: {
-    fontSize: 20,
-  },
-});
 
 function LoginButton(props) {
   const { classes } = props;
   return (
-      <StyledButton color="#000000" variant="outlined">
-      Login
-        <SaveIcon className={classNames(classes.rightIcon, classes.iconSmall)} />
-      </StyledButton>
+      <Button/>
   );
 }
 
-export default withStyles(styles)(LoginButton);
+export default LoginButton;
