7367d3645b9bf4fdec4827bda6aee3b83eea1bbe
manhtho <manhtho@seznam.cz>
2018-11-02 16:03:33 +0100

    Repair loginpage and loginform
    Signed-off-by: manhtho <manhtho@seznam.cz>

--- frontend/src/components/pages/LoginPage.js
+++ frontend/src/components/pages/LoginPage.js
@@ -1,20 +1,39 @@
-import React, {Component} from 'react';
-import LoginForm from '../molecules/LoginForm';
-import {Layout} from '../atoms/Layout';
-import {Link} from '../atoms/Link';
-import Logo from '../atoms/Logo';
-import Typography from '@material-ui/core/Typography';
+import React, { Component } from 'react';
+import { Layout } from '../atoms/Layout';
+import NavigationBar from '../molecules/NavigationBar'
+import { connect } from 'react-redux';
+import { logout } from '../../actions/user.actions';
 
-export class LoginPage extends Component {
+class FirstPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+  const { logout } = this.props;
+  logout();
+  }
+
   render() {
     return (
-      <Layout className="lp-1">
-        <Logo/>
-          <LoginForm/>
-          <Link class="no-underline" to="/dashboard"><Typography align="center">
-            Login as Admin
-          </Typography></Link>
+      <Layout>
+        <div>
+          <NavigationBar user={this.props.user} onClick={this.onClick} />
+        </div>
       </Layout>
-    );
+    )
   }
 }
+
+
+function mapStateToProps(state) {
+    const { user } = state.authentication.user;
+    return { user };
+}
+
+const mapDispatchToProps = { logout };
+
+const connectedFirstPage = connect(mapStateToProps,mapDispatchToProps)(FirstPage);
+export { connectedFirstPage as FirstPage };
