00a487c98196117d1c26099f008d41e3e02de643
unknown <janpippal@gmail.com>
2018-11-07 02:24:21 +0100

    controller adjusted

--- backend/src/modules/catalog/catalogController.js
+++ backend/src/modules/catalog/catalogController.js
@@ -3,15 +3,9 @@ import jwt from 'jsonwebtoken';
 
 export const catalogController = async (req, res, next) => {
 
-  const departments = await db.Department.findAll({ attributes: ['id','name']})
-  .then(departments => { return departments })
-  .catch(() => { return next();});
-  const roles = await db.Role.findAll({ attributes: ['id','name']})
-  .then(roles => { return roles })
-  .catch(() => { return next();});
-  const jobPositions = await db.Job_position.findAll({ attributes: ['id','name']})
-  .then(jobPositions => { return jobPositions })
-  .catch(() => { return next();});
+  const departments = await db.Department.findAll({ attributes: ['id','name']}).then(departments => { return departments }).catch(() => { return '{}' });
+  const roles = await db.Role.findAll({ attributes: ['id','name']}).then(roles => { return roles }).catch(() => { return '{}' });
+  const jobPositions = await db.Job_Position.findAll({ attributes: ['id','name']}).then(jobPositions => { return jobPositions }).catch(() => { return '{}' });
 
 return res.json({
   departments: departments,
