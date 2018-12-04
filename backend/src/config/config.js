e3fbb94c14ff45c45c2bd74b33946c208b2286a4
xdurm25 <44434711+xdurm25@users.noreply.github.com>
2018-12-04 22:50:00 +0100

    Update config.js

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "",
+    "username": "team08",
+    "password": "x44j71fhap1d9f4g",
     "database": "user_team08",
     "host": "localhost",
     "dialect": "mysql"
